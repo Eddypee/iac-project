6957391786ebf3429038a0a8e315791d0e39d0f8
Eddypee <63885682+Eddypee@users.noreply.github.com>
2025-05-13 09:29:27 +0100

    Create variables.tf

---
+++ bootstrap-backend/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region to deploy the backend resources"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "bucket_name" {
+  description = "S3 bucket name for Terraform remote state"
+  type        = string
+  default     = "tf-backend-class38-iac-project"
+}
+
+variable "lock_table" {
+  description = "DynamoDB table for state locking"
+  type        = string
+  default     = "tf-state-lock"
+}
+
+variable "environment" {
+  description = "Environment label for resources"
+  type        = string
+  default     = "dev"
+}
