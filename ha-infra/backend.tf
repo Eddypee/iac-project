8622a2c81df5d810dcad4ea89a1ec3423263d5df
Eddypee <63885682+Eddypee@users.noreply.github.com>
2025-05-13 09:52:59 +0100

    Update backend.tf

--- ha-infra/backend.tf
+++ ha-infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "iac-project-divine-automation-bucket"
+    bucket         = "iac-project-divine-enterprise-bucket"
     key            = "iac-project/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "terraform-state-locking"
