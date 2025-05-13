ee1287058cd67edb24990305e7be116ebd50ec80
Eddypee <63885682+Eddypee@users.noreply.github.com>
2025-05-13 09:22:34 +0100

    Create main.tf
    For backend configuration

---
+++ bootstrap-backend/main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = var.bucket_name
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = var.environment
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = var.lock_table
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = var.environment
+  }
+}
